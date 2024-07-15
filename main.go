2248169d77434f12216535bd3dfe08c34c59d463
Gonzalo Bosio <gonzaabosio@gmail.com>
2024-07-15 01:45:09 -0300

    Connection to CockroachDB

--- main.go
+++ main.go
@@ -1,15 +1,42 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
+	"github.com/gonzabosio/crypto-users/data"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env data:", err)
+	}
+
+	dsn := os.Getenv("CONN_STR")
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("failed to connect database", err)
+	} else {
+		log.Printf("successful database connection")
+	}
+
+	user := new(data.User)
+
+	err = db.AutoMigrate(&data.User{})
+	if err != nil {
+		log.Fatal("auto migration failed")
+	}
+	db.Raw("SELECT username FROM \"User\"").Scan(&user)
+
 	e := echo.New()
 
 	e.GET("/", func(c echo.Context) error { return c.String(http.StatusOK, "Hello world") })
 
-	e.Logger.Fatal(e.Start(":4000"))
+	e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
 }
